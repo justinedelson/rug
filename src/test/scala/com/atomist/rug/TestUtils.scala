b1b8300e4070ebf1ed9b2e4ab4ccbd75bb9d171b
James Carnegie <james@atomist.com>
2017-01-16 16:00:11 +0000

    Generate/Compile Core.ts on the fly instead of using Maven

--- src/test/scala/com/atomist/rug/TestUtils.scala
+++ src/test/scala/com/atomist/rug/TestUtils.scala
@@ -2,11 +2,12 @@ package com.atomist.rug
 
 import java.io.File
 
-import com.atomist.project.ProjectOperationArguments
+import com.atomist.project.{ProjectOperationArguments, SimpleProjectOperationArguments}
 import com.atomist.project.archive.{AtomistConfig, DefaultAtomistConfig}
 import com.atomist.project.edit.{ModificationAttempt, ProjectEditor, SuccessfulModification}
 import com.atomist.rug.compiler.typescript.TypeScriptCompiler
 import com.atomist.rug.kind.DefaultTypeRegistry
+import com.atomist.rug.ts.TypeScriptInterfaceGenerator
 import com.atomist.source.file.{FileSystemArtifactSource, FileSystemArtifactSourceIdentifier}
 import com.atomist.source.{ArtifactSource, SimpleFileBasedArtifactSource}
 import jdk.nashorn.api.scripting.ScriptObjectMirror
@@ -24,7 +25,6 @@ object TestUtils extends Matchers {
 
     attemptModification(program, as, backingAs, poa, pipeline) match {
       case sm: SuccessfulModification =>
-        // show(sm.result)
         sm.result
     }
   }
@@ -46,12 +46,14 @@ object TestUtils extends Matchers {
 
   // This brings in a node_modules directory that was copied there by a maven goal called copy-ts, which takes it from src/main/typescript
   val user_model: ArtifactSource = {
-    val src = new FileSystemArtifactSource(FileSystemArtifactSourceIdentifier(new File("target/.atomist")))
-    val compiled = compiler.compile(src.underPath("node_modules/@atomist").withPathAbove(".atomist"))
+
+    val generator = new TypeScriptInterfaceGenerator
+    val output = generator.generate(SimpleProjectOperationArguments("", Map(generator.OutputPathParam -> "Core.ts")))
+    val src = new FileSystemArtifactSource(FileSystemArtifactSourceIdentifier(new File("src/main/typescript")))
+
+    val compiled = compiler.compile(src.underPath("node_modules/@atomist").withPathAbove(".atomist") + output.withPathAbove(".atomist/rug/model"))
     compiled.underPath(".atomist").withPathAbove(".atomist/node_modules/@atomist")
   }
-
-
 
   def compileWithModel(tsAs: ArtifactSource) : ArtifactSource = {
     compiler.compile(user_model + tsAs)
